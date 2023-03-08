31b32bb1e20b9a1e4dec962060c3019d95c98651
Marvin <ntwcklng@gmail.com>
2023-03-08 18:47:42 +0100

    ?

--- components/card.js
+++ components/card.js
@@ -3,7 +3,7 @@ import { cx } from '@/utils/all'
 export default function Card(props) {
   return (
     <div className='transition-all relative rounded-lg cardBorderGradient'>
-      <div className={cx('  p-8 my-16 bg-black', props.className)}>
+      <div className={cx('p-8 my-16 bg-black', props.className)}>
         {props.children}
       </div>
     </div>
